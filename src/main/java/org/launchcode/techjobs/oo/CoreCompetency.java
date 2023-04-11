7546470919439e3263b3582e932e28d9c02ae38a
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-04-10 21:58:36 -0500

    did the first 2 steps of the assignment

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class CoreCompetency {
 
     private int id;
+
     private static int nextId = 1;
     private String value;
 
@@ -40,5 +41,16 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 
 }
